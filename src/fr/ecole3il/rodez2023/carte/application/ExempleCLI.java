5038be8fe8914b435a73321650778d793d353f4a
Hugo Vacquier <94376539+Hugo-Vacquier@users.noreply.github.com>
2024-03-28 11:01:23 +0100

    Test

--- src/fr/ecole3il/rodez2023/carte/application/ExempleCLI.java
+++ src/fr/ecole3il/rodez2023/carte/application/ExempleCLI.java
@@ -1,30 +1,17 @@
 package fr.ecole3il.rodez2023.carte.application;
 
-import fr.ecole3il.rodez2023.carte.AdaptateurAlgorithme;
-import fr.ecole3il.rodez2023.carte.chemin.algorithmes.AlgorithmeAEtoile;
-import fr.ecole3il.rodez2023.carte.chemin.algorithmes.AlgorithmeChemin;
-import fr.ecole3il.rodez2023.carte.chemin.algorithmes.AlgorithmeDijkstra;
 import fr.ecole3il.rodez2023.carte.elements.Carte;
 import fr.ecole3il.rodez2023.carte.elements.Chemin;
 import fr.ecole3il.rodez2023.carte.manipulateurs.GenerateurCarte;
+import fr.ecole3il.rodez2023.carte.chemin.algorithmes.AlgorithmeDijkstra;
 
-/**
- * @author p.roquart
- * genre on peut tester le code
- * c'est agaçant tous ces tests
- * 
- * de toute façon quand ça compile c'est bon
- */
 public class ExempleCLI {
 
 	public static void main(String[] args) {
-		GenerateurCarte generateur = new GenerateurCarte();
-		Carte test = generateur.genererCarte(100,100);
-		AlgorithmeChemin algoChemin = new AlgorithmeDijkstra();
-		Chemin chemin = algoChemin.trouverChemin(test, 0, 0, 50, 50);
-		chemin.afficherChemin();
-		chemin = algoChemin.trouverChemin(test, 0, 0, 50, 50);
+		Carte carte = GenerateurCarte.genererCarte(20, 20); // Ajustement des dimensions pour variété
+		AlgorithmeDijkstra algorithme = new AlgorithmeDijkstra(); // Utilisation explicite pour simplifier l'exemple
+
+		Chemin chemin = algorithme.trouverChemin(carte, 0, 0, 19, 19); // Changement des coordonnées pour un exemple différent
 		chemin.afficherChemin();
 	}
-	
 }
